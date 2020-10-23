e2dbbe3d4bc42c66bc863d41c559176d1da301a0
Justin Dinkelbach <jmdinkelbach@gmail.com>
2020-10-23 12:21:22 -0700

    60% Drop

--- legacy_routers.py
+++ legacy_routers.py
@@ -17,7 +17,7 @@ def myNetwork():
 
     net = Mininet( topo=None,
                    build=False,
-                   ipBase='10.0.0.0/8')
+                   ipBase='0.0.0.0')
 
     info( '*** Adding controller\n' )
     c0=net.addController(name='c0',
@@ -28,24 +28,24 @@ def myNetwork():
     info( '*** Add switches\n')
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-    r4 = net.addHost('r4', cls=Node, ip='0.0.0.0')
+    r4 = net.addHost('r4', cls=Node, ip='10.0.1.0')
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r3 = net.addHost('r3', cls=Node, ip='192.168.1.1/24')
+    r3 = net.addHost('r3', cls=Node, ip='10.0.2.0')
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r5 = net.addHost('r5', cls=Node, ip='0.0.0.0')
+    r5 = net.addHost('r5', cls=Node, ip='10.0.2.0')
     r5.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.1.0/24', defaultRoute='via 192.168.1.1')
-    h2 = net.addHost('h2', cls=Host, ip='10.0.2.0/24', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.0/24', defaultRoute='via 192.168.1.0')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.1.0/24', defaultRoute='via 192.168.2.0')
 
     info( '*** Add links\n')
-    net.addLink(s1, h1)
-    net.addLink(r3, s1)
-    net.addLink(r3, r4)
-    net.addLink(r4, r5)
-    net.addLink(r5, s2)
-    net.addLink(s2, h2)
+    net.addLink(h1, s1, intfName2='s1-eth1',params2={ 'ip' : ' 10.0.1.0/24'})
+    net.addLink(r3, s1, intfName2='s1-eth2',params2={ 'ip' : ' 10.0.1.0/24'})
+    net.addLink(r3, r4, intfName2='r4-eth2',params2={ 'ip' : '192.168.1.0/24'})
+    net.addLink(r4, r5, intfName2='r4-eth3',params2={ 'ip' : '192.168.2.0/24'})
+    net.addLink(r5, s2, intfName2='s2-eth1',params2={ 'ip' : ' 10.0.2.0/24'})
+    net.addLink(s2, h2, intfName2='s2-eth2',params2={ 'ip' : ' 10.0.2.0/24'})
 
     info( '*** Starting network\n')
     net.build()
@@ -65,4 +65,3 @@ def myNetwork():
 if __name__ == '__main__':
     setLogLevel( 'info' )
     myNetwork()
-
