c7d363bc49b9a81a6dc0c4d261aaeab244dfe888
Andrew <timothyhinea@gmail.com>
2020-10-22 23:47:51 -0700

    70% drop rate

--- legacy_routers.py
+++ legacy_routers.py
@@ -17,7 +17,7 @@ def myNetwork():
 
     net = Mininet( topo=None,
                    build=False,
-                   ipBase='0.0.0.0')
+                   ipBase='10.0.1.1/24')
 
     info( '*** Adding controller\n' )
     c0=net.addController(name='c0',
@@ -26,27 +26,39 @@ def myNetwork():
                       port=6633)
 
     info( '*** Add switches\n')
-    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
-    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-    r4 = net.addHost('r4', cls=Node, ip='0.0.0.0')
-    r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r3 = net.addHost('r3', cls=Node, ip='0.0.0.0')
-    r3.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r5 = net.addHost('r5', cls=Node, ip='0.0.0.0')
-    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+    s2 = net.addSwitch('s2')
+    s1 = net.addSwitch('s1')
+   
+    r4 = net.addHost('r4', cls=Node, ip='192.168.1.1/24')
+    #r4.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
+    #r3.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r5 = net.addHost('r5', cls=Node, ip='10.0.2.1/24')
+    #r5.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1/24', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2/24', defaultRoute=None)
+    h1 = net.addHost( 'h1', ip='10.0.1.100/24',
+                           defaultRoute='10.0.1.100/24' )
+                           
+    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+    h2 = net.addHost('h2', ip='10.0.2.100/24', defaultRoute='10.0.2.100/24')
 
     info( '*** Add links\n')
-    net.addLink(h1, s1, intfName2='s1-eth1',params2={ 'ip' : ' 10.0.1.1/24'})
-    net.addLink(r3, s1, intfName2='s1-eth2',params2={ 'ip' : ' 10.0.1.2/24'})
-    net.addLink(r3, r4, intfName2='r4-eth2',params2={ 'ip' : '192.168.1.0/24'})
-    net.addLink(r4, r5, intfName2='r4-eth3',params2={ 'ip' : '192.168.2.0/24'})
-    net.addLink(r5, s2, intfName2='s2-eth1',params2={ 'ip' : ' 10.0.2.1/24'})
-    net.addLink(s2, h2, intfName2='s2-eth2',params2={ 'ip' : ' 10.0.2.2/24'})
+    net.addLink(s1, r3, intfName2='r3-eth1', params2={ 'ip' : '10.0.1.1/24' } )
+    net.addLink(s1, h1)
+    net.addLink(s2, r5, intfName2='r5-eth1', params2={ 'ip' : '10.0.2.0/24' } )
+    net.addLink(s2, h2)
+    
+    net.addLink(r4, r3, intfName1='r4-eth0', 
+                        intfName2='r3-eth2',   
+                        params1={ 'ip' : '10.0.1.100/24'},
+                        params2={ 'ip' : '192.168.1.1/24' })
 
+    net.addLink(r4, r5, intfName2='r5-eth2', params2={ 'ip' : '192.168.1.1/24' })
+
+    s1.cmd('sysctl -w net.ipv4.ip_forward=1')
+    h1.cmd('sysctl -w net.ipv4.ip_forward=1')
+    s1.cmd('sysctl -w net.ipv4.ip_forward=1')
     info( '*** Starting network\n')
     net.build()
     info( '*** Starting controllers\n')
